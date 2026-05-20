c42d84af915f277425c9d29435860ed2b3cce94c
EC2 Default User <ec2-user@ip-172-31-26-170.us-west-1.compute.internal>
2026-05-20 18:13:03 +0000

    Change subnet availability zone to us-west-1c

--- tests/setup/main.tf
+++ tests/setup/main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "test_vpc" {
 resource "aws_subnet" "test_subnet" {
   vpc_id            = aws_vpc.test_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-1c"
 
   tags = {
     Name = "test-subnet"
